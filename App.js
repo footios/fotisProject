7cd4ff259dc935be74f30430089e748dd4d84d47
Fotis Tsakiris <footios76@gmail.com>
2019-04-24 15:39:12 +0300

    FlatList added

--- App.js
+++ App.js
@@ -10,31 +10,34 @@ import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
-import PlaceList from './src/components/PlaceList/PlaceList'
+import PlaceList from './src/components/PlaceList/PlaceList';
 
 export default class App extends Component {
 	state = {
 		places: []
 	};
 	placeAddedHandler = (placeName) => {
-		this.setState((prevState) => ({ places: prevState.places.concat(placeName) }));
+		this.setState((prevState) => ({
+			places: prevState.places.concat({
+				key: Math.random(),
+				value: placeName
+			})
+		}));
 	};
 
-	placesDeletedItem = (index) => {
-		placesOutput = 
-		this.setState(prevState => {
+	placeDeletedItem = (key) => {
+		placesOutput = this.setState((prevState) => {
 			return {
-				places: prevState.places.filter((place, i) => i !== index)
-			}
-		})
-	 }
- 
+				places: prevState.places.filter((place) => place.key !== key)
+			};
+		});
+	};
 
 	render() {
 		return (
 			<View style={styles.container}>
 				<PlaceInput onPlaceAdded={this.placeAddedHandler} />
-				<PlaceList places={this.state.places} onItemDeleted={this.placesDeletedItem} />
+				<PlaceList places={this.state.places} onItemDeleted={this.placeDeletedItem} />
 			</View>
 		);
 	}
