d367f09e9a07122d705b534cc041de1cf38016b7
Fotis Tsakiris <footios76@gmail.com>
2019-05-01 18:41:24 +0300

    react native navigation to adroid added

--- android/app/src/main/java/com/fotisproject/MainActivity.java
+++ android/app/src/main/java/com/fotisproject/MainActivity.java
@@ -1,15 +1,22 @@
 package com.fotisproject;
 
 import com.facebook.react.ReactActivity;
-
-public class MainActivity extends ReactActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "fotisProject";
-    }
+import com.reactnativenavigation.controllers.SplashActivity;
+ 
+public class MainActivity extends SplashActivity {
+ 
 }
+
+// import com.facebook.react.ReactActivity;
+
+// public class MainActivity extends ReactActivity {
+
+//     /**
+//      * Returns the name of the main component registered from JavaScript.
+//      * This is used to schedule rendering of the component.
+//      */
+//     @Override
+//     protected String getMainComponentName() {
+//         return "fotisProject";
+//     }
+// }
