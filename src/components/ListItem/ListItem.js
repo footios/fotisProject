f119ec1ac1f7a6e0a985456f36489c5a842cda8a
Fotis Tsakiris <footios76@gmail.com>
2019-05-01 10:55:35 +0300

    Redux added

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 
-//child of PlaceList
-const listItem = (props) => {
-	return (
-		<TouchableOpacity onPress={props.onItemPressed}>
-			<View style={styles.listItem}>
-            <Image resizeMode='cover' style={styles.placeImage} source={props.placeImage} />
-				<Text>{props.placeName}</Text>
-			</View>
-		</TouchableOpacity>
-	);
-};
+const listItem = props => (
+  <TouchableOpacity onPress={props.onItemPressed}>
+    <View style={styles.listItem}>
+      <Image resizeMode="cover" source={props.placeImage} style={styles.placeImage} />
+      <Text>{props.placeName}</Text>
+    </View>
+  </TouchableOpacity>
+);
 
 const styles = StyleSheet.create({
-	listItem: {
-		width: '100%',
-		padding: 10,
-		marginBottom: 5,
-        backgroundColor: '#eee',
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    placeImage: {
-		marginRight: 8,
-		// When fetching img from web, must set height/width!
-        height: 30,
-        width: 30
-    }
+  listItem: {
+    width: "100%",
+    marginBottom: 5,
+    padding: 10,
+    backgroundColor: "#eee",
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  placeImage: {
+      marginRight: 8,
+      height: 30,
+      width: 30
+  }
 });
 
 export default listItem;
